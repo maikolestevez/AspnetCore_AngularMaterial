af79f39aba582c5a18e3220e0243f0d819c6dba8
maikolestevez <maikolestevez@gmail.com>
2017-05-27 09:28:28 -0400

    Inserción inicial en el repositorio del módulo AspnetCore_AngularMaterial

---
+++ Program.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Asp_net_Core_Angular
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+				var host = new WebHostBuilder()
+			   .UseKestrel()
+			   .UseContentRoot(Directory.GetCurrentDirectory())
+			   .UseIISIntegration()
+			   .UseStartup<Startup>()
+			   .Build();
+
+				host.Run();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+           
+        }
+    }
+}
